c074156263f9a97f9db31e3067d8128e157a5106
arabenitezz <abenitezr18@gmail.com>
2025-01-18 11:52:51 -0300

    favicon added

--- back/models/comment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  link: { type: mongoose.Schema.Types.ObjectId, ref: 'Link' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', commentSchema);
